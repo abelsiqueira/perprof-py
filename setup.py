e2d9a85a8768a4aaae8e585ba35be786aa43f995
Raniere Silva <raniere@ime.unicamp.br>
2013-11-01 12:31:37 -0200

    Add module and setup
    The module have been create and a setup file for it to.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='perprof',
+    version='0.0.0',
+    author='Abel Soares Siqueira, Raniere Gaia Costa da Silva, Luíz Rafael dos Santos',
+    author_email='abel@ime.unicamp.br, raraniere@ime.unicamp.br',
+    packages=['perprof'],
+    url='https://github.com/abelsiqueira/perprof-py',
+    license='LICENSE',
+    description='A python module for performance profiling (as described by Dolan and Moré)',
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts':['perprof = perprof.main:main']}
+)
