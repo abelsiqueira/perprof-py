ca90442320e3600f1d6aa96c5dcd358e726ac871
Raniere Silva <raniere@ime.unicamp.br>
2013-11-01 20:22:13 -0200

    Add new arguments
    * Add arguments to console command.
    * Add methods to the class that store the arguments.

--- perprof/main.py
+++ perprof/main.py
@@ -2,13 +2,35 @@
 
 class PerProfSetup():
     """This is a class to store the files to be used."""
-    pass
+    def __init__(self):
+        self.cache = False
+        self.files = []
+
+    def using_cache(self):
+        return self.cache
+
+    def set_cache(val):
+        self.cache = val
+
+    def get_files(self):
+        return self.files
+
+    def set_files(files):
+        self.files = files
 
 def main():
     """This is the entry point when calling perprof."""
     import argparse
 
     parser = argparse.ArgumentParser(
-            description='A python module for performance profiling (as described by Dolan and Moré)')
+            description='A python module for performance profiling (as described by Dolan and Moré).')
+    parser.add_argument('-c', '--cache', action='store_true',
+            help='Enable cache.')
+    parser.add_argument('file_name', nargs='+',
+            help='The name of the files to be used for the performance profiling')
 
     args = parser.parse_args()
+
+    s = PerProfSetup()
+    s.set_cache(args.cache)
+    s.set_files(args.file_name)
