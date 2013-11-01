7e7ce40b85a63f286198963aa5aa42e26c792c11
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2013-11-01 12:32:54 -0200

    Correct Raniere's e-mail in setup.py
    Correct Raniere's e-mail in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='perprof',
     version='0.0.0',
     author='Abel Soares Siqueira, Raniere Gaia Costa da Silva, Luíz Rafael dos Santos',
-    author_email='abel@ime.unicamp.br, raraniere@ime.unicamp.br',
+    author_email='abel@ime.unicamp.br, raniere@ime.unicamp.br',
     packages=['perprof'],
     url='https://github.com/abelsiqueira/perprof-py',
     license='LICENSE',
