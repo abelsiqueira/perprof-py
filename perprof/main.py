e6b66a08685402a1eb249d33ed347075dc149c81
Raniere Silva <raniere@ime.unicamp.br>
2013-11-05 18:22:52 -0200

    Improves after merge
    * The arguments for `__init__` of PerProfSetup is now just args
      (from argparse)
    * Add option for logarithmic scale
    * Add options for matplotlib and LaTeX/TikZ (they are exclusive)
    * Create module for LaTeX/TikZ plot function
    * Move examples from `tikzexample` to `test`
    * Remove `tikzexample/profile.tex` since it was the output of this
      module
    * Remove `tikzexample/examples.tex` since the user must know how
      to write it.
    
    TODO:
    
    * Add function to write the main document for the LaTeX/TikZ
    * Implement the logarithmic scale option for matplotlib

--- perprof/main.py
+++ perprof/main.py
@@ -2,9 +2,10 @@
 
 class PerProfSetup():
     """This is a class to store the files to be used."""
-    def __init__(self, fnames, cache=False):
-        self.cache = cache
-        self.files = fnames
+    def __init__(self, args):
+        self.cache = args.cache
+        self.files = args.file_name
+        self.log = args.log
 
     def using_cache(self):
         return self.cache
@@ -18,13 +19,25 @@ class PerProfSetup():
     def set_files(self, files):
         self.files = files
 
+    def using_log(self):
+        return self.log
+
+    def set_log(self, val):
+        self.log= val
+
 def main():
     """This is the entry point when calling perprof."""
     import argparse
-    from . import matplotlib
 
     parser = argparse.ArgumentParser(
             description='A python module for performance profiling (as described by Dolan and Moré).')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--mp', action='store_true',
+            help='Use matplotlib as backend for the plot')
+    group.add_argument('--tikz', action='store_true',
+            help='Use LaTex/TikZ as backend for the plot (only generate the TeX file)')
+    parser.add_argument('--log', action='store_true',
+            help='Use logarithmic scale for the plot.')
     parser.add_argument('-c', '--cache', action='store_true',
             help='Enable cache.')
     parser.add_argument('file_name', nargs='+',
@@ -32,12 +45,17 @@ def main():
 
     args = parser.parse_args()
 
-    s = PerProfSetup(args.file_name, args.cache)
-    # With matplotlib
-    d = matplotlib.MPdata(s)
+    s = PerProfSetup(args)
+
+    if s.mp:
+        # matplotlib
+        from . import matplotlib
+
+        d = matplotlib.Profiler(s)
+    elif s.tikz:
+        # tikz
+        from . import tikz
+
+        d = tikz.Profiler(s)
+
     d.plot()
-    # With tikz
-    d = prof.Pdata(s)
-    d.scale()
-    d.generate_perf_functions()
-    d.print_tikz(True)
