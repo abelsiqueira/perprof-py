ce74d733a7651645de292d966fe4150272d62891
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2013-11-07 09:44:43 -0200

    Implement the subset flag
    Using --subset <file> will compare only with the problems inside the
    given file. The file is given to the parser, which opens it only once.

--- perprof/prof.py
+++ perprof/prof.py
@@ -13,9 +13,13 @@ def load_data(setup):
     :param setup: the setup configurations
     :type setup: main.PerProfSetup
     """
+    try:
+        subset = [l.strip() for l in open(setup.get_subset(),'r')]
+    except:
+        subset = []
     data = {}
     for f in setup.get_files():
-        data_tmp, solver_name = parse.parse_file(f)
+        data_tmp, solver_name = parse.parse_file(f, subset)
         data[solver_name] = data_tmp
     return data
 
