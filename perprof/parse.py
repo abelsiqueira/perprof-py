61ab4d717cd67fa1d169d031161043873baccbe4
Raniere Silva <raniere@ime.unicamp.br>
2013-11-11 12:32:19 -0200

    Improves in sanitize
    * Rename `problem_name_sanitize()` function to `str_sanitize()`.
    * Use `ldata[0] = str_sanitize(ldata[0])`.
    * Add `sanitize.sample` file.
    * Sanitize `algname`.
    * Use `str.strip()` method.
    
    All the above changes was request by @abelsiqueira.

--- perprof/parse.py
+++ perprof/parse.py
@@ -10,12 +10,13 @@ The files must be in the following format::
     problem-name d
     problem-name c time-in-seconds
 
-The ``c`` in the second column means that the problem converged and the ``d``
-that it hadn't (in this case ``time-in-seconds`` is optional and can be a string
-since it will be stored as ``float("inf")``).
+The first column is the problem name.  The second column indicates if the
+problem converged, where ``c`` means yes, and ``d`` means no.  The third column
+is the elapsed time the solver used to reach the solution. If the solver did not
+converge, this column is ignored.
 """
 
-def problem_name_sanitize(name):
+def str_sanitize(name):
     """
     This sanitize the problem name for LaTeX.
     """
@@ -28,22 +29,23 @@ def parse_file(fname):
     This function parse one file.
     """
     data = {}
-    algname = fname
+    algname = str_sanitize(fname)
     with open(fname) as f:
         for l in f:
             ldata = l.split()
             if ldata[0] == '#Name' and len(ldata) == 2:
-                algname = ldata[1]
+                algname = str_sanitize(ldata[1])
             elif len(ldata) < 2:
-                raise ValueError('Line must have at least 2 elements: `{}`.'.format(l[:-1]))
+                raise ValueError('Line must have at least 2 elements: `{}`.'.format(l.strip()))
             else:
+                ldata[0] = str_sanitize(ldata[0])
                 if ldata[1] == 'c':
                     if len(ldata) < 3:
-                        raise ValueError('When problem converge line must have at least 3 elements: `{}`.'.format(l[:-1]))
+                        raise ValueError('When problem converge line must have at least 3 elements: `{}`.'.format(l.strip()))
                     else:
-                        data[problem_name_sanitize(ldata[0])] = float(ldata[2])
+                        data[ldata[0]] = float(ldata[2])
                 elif ldata[1] == 'd':
-                    data[problem_name_sanitize(ldata[0])] = float('inf')
+                    data[ldata[0]] = float('inf')
                 else:
                     raise ValueError('The second element in the lime must be `c` or `d`: `{}`.'.format(l[:-1]))
     return data, algname
