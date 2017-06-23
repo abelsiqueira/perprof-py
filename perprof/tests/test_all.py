a993626cb36a4296f820c613aea70bd93535cd51
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-06-23 17:52:27 -0300

    Fix tests

--- perprof/tests/test_all.py
+++ perprof/tests/test_all.py
@@ -52,22 +52,21 @@ class TestPerprof(unittest.TestCase):
                 self.assertEqual(options['output_format'], output)
                 self.backend_plots[backend](x, y, options)
 
-'''
     def test_only_name(self):
         for p in self.profiles:
-            args = '--' + self.profile_args[p] + ' --tikz perprof/tests/only-name.sample ' + self.goodfiles
+            args = self.profile_args[p] + ' --tikz perprof/tests/only-name.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
             self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_columns(self):
-        for backend in self.backends:
-            baseargs = '--' + backend + ' ' + self.goodfiles
+        for p in self.profiles:
+            baseargs = self.profile_args[p] + ' --tikz ' + self.goodfiles
             #Default comparison needs 3 columns
             args = baseargs + ' perprof/tests/2-col.sample '
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
+            self.assertRaises(ValueError, self.profiles[p], options)
             #Default values should fail with 5 or less columns.
             #Unconstrained Default values should fail with 5 or less columns
             #(because dual default column is 5).
@@ -78,75 +77,69 @@ class TestPerprof(unittest.TestCase):
                     args = baseargs + ' perprof/tests/{}-col.sample '.format(n)
                     args = set_arguments(args.split())
                     options = process_arguments(args)
-                    self.assertRaises(ValueError, self.back_profilers[backend], options)
+                    self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_without_time(self):
-        for backend in self.backends:
-            args = '--' + backend + ' perprof/tests/without-time.sample ' + self.goodfiles
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz perprof/tests/without-time.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
+            self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_without_c_or_d(self):
-        for backend in self.backends:
-            args = '--' + backend + ' perprof/tests/c-or-d.sample ' + self.goodfiles
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz perprof/tests/c-or-d.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
+            self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_zero_time(self):
         for p in self.profiles:
-            for backend in self.backends:
-                args = '--' + self.profile_args[p] + ' --' + backend + \
-                        'perprof/tests/zero-time.sample ' + self.goodfiles
-                args = set_arguments(args.split())
-                options = process_arguments(args)
-                self.assertRaises(ValueError, self.back_profilers[backend], options)
-
-    def test_yaml_fail(self):
-        for backend in self.backends:
-            args = '--' + backend + ' perprof/tests/yaml-fail.sample ' + self.goodfiles
+            args = self.profile_args[p] + ' --tikz perprof/tests/zero-time.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
+            self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_empty_file(self):
-        for backend in self.backends:
-            args = '--' + backend + ' perprof/tests/empty.sample ' + self.goodfiles
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz perprof/tests/empty.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
+            self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_empty_subset(self):
-        for backend in self.backends:
-            args = '--' + backend + ' --demo --subset perprof/tests/empty.subset'
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz --demo --subset perprof/tests/empty.subset'
             args = set_arguments(args.split())
             self.assertRaises(AttributeError, process_arguments, args)
 
     def test_empty_intersection(self):
-        for backend in self.backends:
-            args = '--' + backend + ' --demo --subset perprof/tests/fantasy.subset'
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz --demo --subset perprof/tests/fantasy.subset'
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
+            self.assertRaises(ValueError, self.profiles[p], options)
 
     def test_no_success(self):
-        for backend in self.backends:
-            if backend == "raw":
-                continue
-            args = '--' + backend + ' perprof/tests/no-success.sample ' + self.goodfiles
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz perprof/tests/no-success.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
-            data = self.back_profilers[backend](options)
-            self.assertRaises(ValueError, data.plot)
+            prof = self.profiles[p](options)
+            self.assertRaises(ValueError, prof.compute_profile)
 
     def test_repeated_problem(self):
-        for backend in self.backends:
-            args = '--' + backend + ' perprof/tests/repeat.sample ' + self.goodfiles
+        for p in self.profiles:
+            args = self.profile_args[p] + ' --tikz perprof/tests/repeat.sample ' + self.goodfiles
             args = set_arguments(args.split())
             options = process_arguments(args)
-            self.assertRaises(ValueError, self.back_profilers[backend], options)
-    '''
+            self.assertRaises(ValueError, self.profiles[p], options)
+
+    def test_yaml_fail(self):
+        args = '--tikz perprof/tests/yaml-fail.sample ' + self.goodfiles
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, perfprof.PerfProfile, options)
 
 if __name__ == '__main__':
     unittest.main()
