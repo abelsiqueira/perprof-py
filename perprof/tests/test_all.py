c220bf626470a1fdd32a466a88581bda20925d27
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-06-25 10:06:00 -0300

    Update tests

--- perprof/tests/test_all.py
+++ perprof/tests/test_all.py
@@ -6,11 +6,15 @@ from perprof import bokeh
 from perprof import tikz
 from perprof import matplotlib
 from perprof import perfprof
+from perprof import dataprof
 
 class TestPerprof(unittest.TestCase):
 
-    goodfiles = ' '.join(['perprof/examples/' + s + '.table' \
-            for s in ['alpha', 'beta', 'gamma']])
+    fileargs = {
+            "perf": ' '.join(['perprof/examples/' + s + '.table' for s in ['alpha', 'beta', 'gamma']]),
+            "data": ' '.join(['perprof/examples/' + s for s in ['df-alpha', 'df-beta']]) + \
+                    ' --problem-sizes perprof/examples/df.sizes'
+            }
     backends = ['bokeh', 'tikz', 'mp', 'raw']
     backend_plots = {
             "bokeh": bokeh.plot,
@@ -18,10 +22,12 @@ class TestPerprof(unittest.TestCase):
             "mp": matplotlib.plot
             }
     profiles = {
-            "perf": perfprof.PerfProfile
+            "perf": perfprof.PerfProfile,
+            "data": dataprof.DataProfile
             }
     profile_args = {
-            "perf": "--performance-profile"
+            "perf": "--performance-profile",
+            "data": "--data-profile"
             }
 
     def test_backends(self):
@@ -52,91 +58,89 @@ class TestPerprof(unittest.TestCase):
                 self.assertEqual(options['output_format'], output)
                 self.backend_plots[backend](x, y, options)
 
-    def test_only_name(self):
+    def test_profiles(self):
         for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/only-name.sample ' + self.goodfiles
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_columns(self):
-        for p in self.profiles:
-            baseargs = self.profile_args[p] + ' --tikz ' + self.goodfiles
-            #Default comparison needs 3 columns
-            args = baseargs + ' perprof/tests/2-col.sample '
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-            #Default values should fail with 5 or less columns.
-            #Unconstrained Default values should fail with 5 or less columns
-            #(because dual default column is 5).
-            baseargs = '--compare optimalvalues ' + baseargs
-            for xtra in ['', '--unconstrained ']:
-                baseargs = xtra + baseargs
-                for n in [2,3,4,5]:
-                    args = baseargs + ' perprof/tests/{}-col.sample '.format(n)
-                    args = set_arguments(args.split())
-                    options = process_arguments(args)
-                    self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_without_time(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/without-time.sample ' + self.goodfiles
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_without_c_or_d(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/c-or-d.sample ' + self.goodfiles
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_zero_time(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/zero-time.sample ' + self.goodfiles
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_empty_file(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/empty.sample ' + self.goodfiles
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_empty_subset(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz --demo --subset perprof/tests/empty.subset'
-            args = set_arguments(args.split())
-            self.assertRaises(AttributeError, process_arguments, args)
-
-    def test_empty_intersection(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz --demo --subset perprof/tests/fantasy.subset'
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
-
-    def test_no_success(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/no-success.sample ' + self.goodfiles
+            args = self.profile_args[p] + ' --tikz -o tmp -f ' + self.fileargs[p]
             args = set_arguments(args.split())
             options = process_arguments(args)
             prof = self.profiles[p](options)
-            self.assertRaises(ValueError, prof.compute_profile)
+
+    # Performance profile tests
+    def test_only_name(self):
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/only-name.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_columns(self):
+        baseargs = self.profile_args["perf"] + ' --tikz ' + self.fileargs["perf"]
+        #Default comparison needs 3 columns
+        args = baseargs + ' perprof/tests/2-col.sample '
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+        #Default values should fail with 5 or less columns.
+        #Unconstrained Default values should fail with 5 or less columns
+        #(because dual default column is 5).
+        baseargs = '--compare optimalvalues ' + baseargs
+        for xtra in ['', '--unconstrained ']:
+            baseargs = xtra + baseargs
+            for n in [2,3,4,5]:
+                args = baseargs + ' perprof/tests/{}-col.sample '.format(n)
+                args = set_arguments(args.split())
+                options = process_arguments(args)
+                self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_without_time(self):
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/without-time.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_without_c_or_d(self):
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/c-or-d.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_zero_time(self):
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/zero-time.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_empty_file(self):
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/empty.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_empty_subset(self):
+        args = self.profile_args["perf"] + ' --tikz --demo --subset perprof/tests/empty.subset'
+        args = set_arguments(args.split())
+        self.assertRaises(AttributeError, process_arguments, args)
+
+    def test_empty_intersection(self):
+        args = self.profile_args["perf"] + ' --tikz --demo --subset perprof/tests/fantasy.subset'
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
+
+    def test_no_success(self):
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/no-success.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        prof = self.profiles["perf"](options)
+        self.assertRaises(ValueError, prof.compute_profile)
 
     def test_repeated_problem(self):
-        for p in self.profiles:
-            args = self.profile_args[p] + ' --tikz perprof/tests/repeat.sample ' + self.goodfiles
-            args = set_arguments(args.split())
-            options = process_arguments(args)
-            self.assertRaises(ValueError, self.profiles[p], options)
+        args = self.profile_args["perf"] + ' --tikz perprof/tests/repeat.sample ' + self.fileargs["perf"]
+        args = set_arguments(args.split())
+        options = process_arguments(args)
+        self.assertRaises(ValueError, self.profiles["perf"], options)
 
     def test_yaml_fail(self):
-        args = '--tikz perprof/tests/yaml-fail.sample ' + self.goodfiles
+        args = '--tikz perprof/tests/yaml-fail.sample ' + self.fileargs["perf"]
         args = set_arguments(args.split())
         options = process_arguments(args)
         self.assertRaises(ValueError, perfprof.PerfProfile, options)
