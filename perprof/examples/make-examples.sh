09da6730ef2a39b580c7d7e2ff741f550b7f1f10
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2017-06-25 10:34:39 -0300

    Update make-examples.sh

--- perprof/examples/make-examples.sh
+++ perprof/examples/make-examples.sh
@@ -27,23 +27,25 @@ mkdir -p plots
 
 args="-l $lang --pgfplotcompat 1.5"
 
-for backend in --tikz --mp
+for p in --performance-profile --data-profile
 do
-  perprof $backend $args --demo -o plots/abc
-  perprof $backend $args --demo --semilog -o plots/abc-semilog
-  perprof $backend $args --demo --semilog --black-and-white -o \
-    plots/abc-semilog-bw
-  perprof $backend $args --demo --semilog --subset hs.subset -o \
-    plots/abc-semilog-hs
-  perprof $backend $args --demo --tau 100 --semilog -o plots/abc-100
-  perprof $backend $args --demo --maxtime 100 --semilog -o plots/abc-t100
-  perprof $backend $args --demo --mintime 1 --semilog -o plots/abc-m1
-  perprof $backend $args --demo --background 255,255,255 --semilog \
-    -o plots/abc-whiteplot
-  perprof $backend $args --demo --page-background 0,0,0 --semilog \
-    -o plots/abc-blackpage
-  perprof $backend $args --demo --page-background 0,0,0 --background \
-    255,255,255 --semilog -o plots/abc-blackpage-whiteplot
-  perprof $backend $args --demo --semilog --compare optimalvalues \
-    --infeasibility-tolerance 1e-8 -o plots/abc-optimalvalues
+  suffix=$(echo $p | sed 's/--\(.*\)-.*/\1/g')
+  for backend in --tikz --mp
+  do
+    perprof $p $backend $args --demo -o plots/abc-$suffix
+    perprof $p $backend $args --demo --semilog -o plots/abc-semilog-$suffix
+    perprof $p $backend $args --demo --semilog --black-and-white -o plots/abc-semilog-bw-$suffix
+    perprof $p $backend $args --demo --background 255,255,255 --semilog -o plots/abc-whiteplot-$suffix
+    perprof $p $backend $args --demo --page-background 0,0,0 --semilog -o plots/abc-blackpage-$suffix
+    perprof $p $backend $args --demo --page-background 0,0,0 --background 255,255,255 --semilog -o plots/abc-blackpage-whiteplot-$suffix
+
+    if [ $suffix == "performance" ]; then
+      # Specific for performance profile (or not implemented for others yet)
+      perprof $p $backend $args --demo --semilog --subset hs.subset -o plots/abc-semilog-hs-$suffix
+      perprof $p $backend $args --demo --tau 100 --semilog -o plots/abc-100-$suffix
+      perprof $p $backend $args --demo --maxtime 100 --semilog -o plots/abc-t100-$suffix
+      perprof $p $backend $args --demo --mintime 1 --semilog -o plots/abc-m1-$suffix
+      perprof $p $backend $args --demo --semilog --compare optimalvalues --infeasibility-tolerance 1e-8 -o plots/abc-optimalvalues-$suffix
+    fi
+  done
 done
